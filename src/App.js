fec6ad8c648293c8ea03d972d52735d786a8c255
uttamsaha <uttamsaha982@gmail.com>
2022-05-26 11:33:59 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
