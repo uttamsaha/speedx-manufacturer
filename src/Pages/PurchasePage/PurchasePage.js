5a0e1a39b3e69e059ff2ce3e77d0710c62f98542
uttamsaha <uttamsaha982@gmail.com>
2022-05-29 12:29:39 +0600

    bug fixed

--- src/Pages/PurchasePage/PurchasePage.js
+++ src/Pages/PurchasePage/PurchasePage.js
@@ -7,6 +7,9 @@ import auth from '../../firebase.init';
 import './PurchasePage.css';
 
 const PurchasePage = () => {
+  const [isTrue, setTrue] = useState(false);
+  const [newQuantity, setNewQuantity] = useState(0);
+  const [error, setError] = useState('');
     const {id} = useParams();
     const [user] = useAuthState(auth);
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
@@ -19,12 +22,26 @@ const PurchasePage = () => {
         .then(data => setTool(data))
     },[id])
 
+
+    const handleQuantity = (event) => {
+      const quantity = event.target.value;
+      if(quantity >= tool?.minOrderQuantity && quantity <= tool?.available){
+        setTrue(true);
+        setNewQuantity(quantity);
+        setError('');
+      }
+      else{
+        setTrue(false);
+        setNewQuantity(0);
+        setError(`Minimum Order ${tool.minOrderQuantity} and Maximum Order ${tool?.available}`)
+      }
+    }
     const handleOrder = event => {
       event.preventDefault();
       const name = event.target.name.value;
       const email = event.target.email.value;
       const product = event.target.product.value;
-      const quantity = event.target.quantity.value;
+      const quantity = newQuantity;
       const address = event.target.address.value;
       const phone = event.target.phone.value;
 
@@ -64,21 +81,21 @@ const PurchasePage = () => {
                 <p className='tool-price'>Price: <span className='text-primary'>{tool?.price}$/pcs</span></p>
                 <p>{tool?.description?.slice(0,100)}</p>
                 <p className='tool-min-order'>Minimum Order Quantity: <span className="text-primary">{tool.minOrderQuantity} pcs</span></p>
-                {/* <p className='tool-available'>Available: <span className="text-primary">{} pcs</span></p> */}
                 <p className='tool-available'>Total Price: <span className="text-primary">{tool?.available} pcs</span></p>
             </div>
         </div>
       </div>
 
       <form className='flex flex-col justify-center items-center mt-5 lg:w-96 xs:w-xs' onSubmit={handleOrder}>
-      <input type="number" className='input input-bordered w-full max-w-lg mb-3' name='quantity'  placeholder="Enter quantity" required/>
+      <input type="number" className='input input-bordered w-full max-w-lg mb-3' onChange={handleQuantity}  placeholder="Enter quantity" required/> 
+      <span className='text-red-500 text-sm mb-2'>{error}</span>
       <input className='input input-bordered w-full max-w-lg mb-3' name='product'  value={productName} disabled/>
       <input className='input input-bordered w-full max-w-lg mb-3' name='email'value={user?.email} disabled/>
       <input className='input input-bordered w-full max-w-lg mb-3' name='name'  value={user?.displayName} disabled/>
       {/* <input className='input input-bordered w-full max-w-lg mb-3' {...register("age", { min: 18, max: 99 })} placeholder="Enter address"/> */}
       <input className='input input-bordered w-full max-w-lg mb-3' name='address' placeholder="Enter address" required/>
       <input className='input input-bordered w-full max-w-lg mb-3' placeholder="Enter Phone Number" name='phone' required/>
-      <input className='btn btn-primary text-white lg:w-96 xs:w-xs' type="submit" value="Place Order" />
+      <input className='btn btn-primary text-white lg:w-96 xs:w-xs' type="submit" value="Place Order" disabled={!isTrue} />
     </form>
     </div>
   )
